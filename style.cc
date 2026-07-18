34e8d0aff93250859ac55a975e68fc002f7a7947
nam329 <dgim60885@gmail.com>
2026-07-19 00:38:55 +0900

    Delete blink animation keyframes
    Removed blink animation keyframes from style.cc

--- style.cc
+++ style.cc
@@ -57,17 +57,3 @@
     text-shadow:0 0 10px #ff3333;
     animation: blink 1s ease-in-out infinite;
 }
-
-@keyframes blink{
-    0%{
-        opacity:1;
-    }
-
-    50%{
-        opacity:0.3;
-    }
-
-    100%{
-        opacity:1;
-    }
-}
