f194ee6351391920bc0b32ba81b8b6758a563694
nam329 <dgim60885@gmail.com>
2026-07-19 00:20:12 +0900

    Create style.cc

---
+++ style.cc
@@ -0,0 +1,16 @@
+.menu{
+    display:grid;
+    grid-template-columns:repeat(2,1fr);
+    gap:15px;
+    padding:20px;
+}
+
+.menu button{
+    width:100%;
+    height:90px;
+    font-size:24px;
+    border:none;
+    border-radius:15px;
+    background:#222;
+    color:white;
+}
