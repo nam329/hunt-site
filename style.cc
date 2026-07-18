92c6b14f69d39cd91aac4f10f88ab728d8cc565c
nam329 <dgim60885@gmail.com>
2026-07-19 00:36:48 +0900

    Add styling for making class in style.cc

--- style.cc
+++ style.cc
@@ -4,3 +4,11 @@
     gap:15px;
     padding:20px;
 }
+.making{
+    text-align:center;
+    font-size:40px;
+    font-weight:bold;
+    color:#ff3333;
+    margin:20px 0 35px 0;
+    text-shadow:0 0 10px #ff3333;
+        }
